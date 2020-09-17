4a318617258856d353c57903f7890906a94252a9
Gilio Simione <gilio_simione@libero.it>
2020-09-17 17:59:23 +0200

    Registro

--- src/app/pages/register/register-component/register-component.component.ts
+++ src/app/pages/register/register-component/register-component.component.ts
@@ -19,9 +19,20 @@ export class RegisterComponentComponent implements OnInit {
   }
 
   register(): void{
-    this.servicioRegistro.register(this.usuario);
-    window.alert ('Usuario ' + this.usuario.email + ' registrado correctamente');
-    this.router.navigate(['users']) 
+    if(this.isEmpty(this.usuario.email) || this.isEmpty(this.usuario.password)){
+      window.alert ('!Introducir usuario y password¡');
+    }else{
+      this.servicioRegistro.register(this.usuario);
+      window.alert ('Usuario ' + this.usuario.email + ' registrado correctamente');
+      this.router.navigate(['users']) 
+    }
   } 
+  isEmpty(obj: any) {
+    for(var prop in obj) {
+        if(obj.hasOwnProperty(prop))
+            return false;
+    }
 
+    return true;
+}
 }
