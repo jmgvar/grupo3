58190f83dd9e7e3a67183cd0a73e78731c5ec236
Rafael García León <rafa.garcia.leon@gmail.com>
2020-09-17 17:57:22 +0200

    List recursos

--- src/app/pages/resource/listado-recursos-component/listado-recursos-component.component.ts
+++ src/app/pages/resource/listado-recursos-component/listado-recursos-component.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Recurso } from "../../../model/recurso";
+import { ServicioRecursoService} from "../../../services/servicio-recurso.service"
 
 @Component({
   selector: 'app-listado-recursos-component',
@@ -7,9 +10,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListadoRecursosComponentComponent implements OnInit {
 
-  constructor() { }
+  constructor(private recursoService: ServicioRecursoService) { }
+
+  recursos: Observable<any[]>;
 
   ngOnInit(): void {
+    this.recursoService.getAll().subscribe(data => {
+      this.recursos = data['data'];
+      console.log(this.recursos)
+    });
   }
 
 }
--- src/app/services/servicio-recurso.service.ts
+++ src/app/services/servicio-recurso.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import {environment} from '../../environments/environment'
 import { Recurso } from '../model/recurso';
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -11,12 +12,14 @@ export class ServicioRecursoService {
 
   constructor(private http: HttpClient) {}
 
+  
 
-  get(id: String){
+
+  get(id: String) {
     this.http.get(environment.reqresApi+'unknown/'+id);
   }
 
-  getAll(){
-    this.http.get(environment.reqresApi+'unknown');
+  getAll(): Observable<any>{
+    return this.http.get(environment.reqresApi+'unknown');
   }
 }
